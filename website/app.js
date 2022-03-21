5b97b9e57a6b20bdcc04780103ef802563c2287c
Lukescott78 <55175950+Lukescott78@users.noreply.github.com>
2022-03-21 13:31:04 +1000

    Add files via upload

---
+++ website/app.js
@@ -0,0 +1,42 @@
+// Personal API Key for OpenWeatherMap API
+const apiKey = '&appid=4ba79dfb82caa1589f048952db3f6d9f';
+const baseURL = 'https://api.openweathermap.org/='
+const newEntryHolder = document.getElementById('entryHolder').value;
+// Event listener to add function to existing HTML DOM element
+/* Function called by event listener */
+document.getElementById('generate').addEventListener('click', performAction);
+/* Function to GET Web API Data*/
+function performAction(e){
+  getJournalEntry(newEntryHolder, apiKey)
+} 
+/* Function to POST data */
+const getJournalEntry = async (url = '', data = {})=>{
+
+    const response = await fetch(url, {
+      method:'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    try {
+      const newData = await response.json();
+        return newData
+    }catch(error){
+      console.log("error", error);
+    }
+}
+
+// postData('/addEntry', {date: 26/02/2009, temp: 30"c", content: "old and hot"})
+
+/* Function to GET Project Data */
+
+
+
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
